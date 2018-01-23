51c03c01045a60393043eb68771c6f84edc4be96
James Roller <james.roller@aspencapital.com>
2018-01-22 18:50:02 -0800

    input and print a file by non-blank lines

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+#!/usr/bin/env node
+
 'use strict';
 
-console.log('hi');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('graceful-fs'));
+
+const args = process.argv.slice(2);
+console.log(args);
+fs.readFileAsync(args[0], { encoding: 'utf8' })
+    .then(file => {
+        const lines = file
+            .split(/\r?\n/)                   // split on newline
+            .filter(x => !/^\s*$/.test(x));   // remove all blank lines
+
+        console.log(lines);
+    });
