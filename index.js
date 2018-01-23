b84203844e58f4928e6ab81b378d73b4d10d0235
James Roller <james.roller@aspencapital.com>
2018-01-22 19:07:03 -0800

    added parsing of transactions by code and date

--- index.js
+++ index.js
@@ -4,14 +4,35 @@
 
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('graceful-fs'));
+const moment = require('moment');
 
 const args = process.argv.slice(2);
 console.log(args);
+
 fs.readFileAsync(args[0], { encoding: 'utf8' })
     .then(file => {
         const lines = file
             .split(/\r?\n/)                   // split on newline
             .filter(x => !/^\s*$/.test(x));   // remove all blank lines
 
-        console.log(lines);
+        lines.forEach((line, index) => {
+
+            // Detect Transactions
+            const match = /^(\d{6})\s+(\w{1,3})\s+(\d{6})\s+(.*)/.exec(line);
+            const nextMatch = /^\s{17,}(.*)/.exec(lines[index + 1]);
+            if (match) {
+                const postDate = moment(match[1], 'MMDDYY');
+                const code = match[2];
+                const dueDate = moment(match[3], 'MMDDYY');
+                const rest0 = match[4].trim();
+
+                console.log(`code: ${code.padEnd(3)} postDate: ${postDate.format('YYYY-MM-DD')} dueDate: ${dueDate.format('YYYY-MM-DD')}`);
+                if (rest0) {
+                    console.log(`    >${rest0}`);
+                }
+                if (nextMatch) {
+                    console.log(`    >${nextMatch[1]}`);
+                }
+            }
+        });
     });
